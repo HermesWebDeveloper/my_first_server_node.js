fbdd4402982530a65c796b59bd9c38591c61cc67
HERMES BARBOSA  PEREIRA <HERMES6313@DIGITALCOLLEGE.LOCAL>
2024-08-06 09:06:28 -0300

    Inicio do projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql2')
+const PORT = 3000;
+
+var app = express();
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'dc'
+});
+
+con.connect( (err) => {
+    if (err) throw err;
+    console.log('Conectado ao banco de dados!');
+});
+
+app.get('/', (req, res) => {
+    res.send("Bem vindo a nossa Homepage!");
+});
+
+app.listen(PORT, () => {
+    console.log("Conectado na porta: " + PORT);
+});
